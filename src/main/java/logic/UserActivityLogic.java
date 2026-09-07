7695aab98c4099fb3c803dc12a66a9c596499f54
AcxesLo <steve.klein.98@web.de>
2026-09-07 18:20:19 +0200

    cleaned up main

--- src/main/java/logic/UserActivityLogic.java
+++ src/main/java/logic/UserActivityLogic.java
@@ -1,8 +1,53 @@
 package logic;
 
+import api.GitHubAPI;
+import com.google.gson.JsonSyntaxException;
+import json.ConvertResponse;
+import json.FileWriterGson;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class UserActivityLogic {
+    public static GitHubAPI gitHubAPI = new GitHubAPI();
+    public static ConvertResponse convertResponse = new ConvertResponse();
+    public static FileWriterGson fileWriterGson = new FileWriterGson();
+    public static Scanner scanner = new Scanner(System.in);
+    public static String[] parts;
+    public static String githubUser = "";
+    public static String joinedInput;
 
-    public void fetchUser() {
+    public static void fetchUser() {
+        while (true) {
+            try {
+                System.out.print("> ");
 
+                githubUser = scanner.nextLine();
+                parts = githubUser.split("\\s+");
+
+                if (githubUser.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                if (parts[0].equalsIgnoreCase("user-activity")) {
+                    joinedInput = String.join(" ", Arrays.copyOfRange(parts, 1, parts.length));
+                    joinedInput = joinedInput.replaceAll("^\"|\"$", "");
+                    githubUser = joinedInput;
+
+                    gitHubAPI.requestAPI(githubUser);
+                    convertResponse.formattedResponse();
+                    fileWriterGson.writeFile();
+
+                } else {
+                    System.out.println("Wrong command.");
+                }
+
+
+
+            } catch (IOException | InterruptedException | IllegalStateException | JsonSyntaxException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,57 +1,14 @@
 package main;
 
-import api.GitHubAPI;
-import com.google.gson.*;
-import json.ConvertResponse;
-import json.FileWriterGson;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import logic.UserActivityLogic;
 
 public class Main {
-    public static GitHubAPI gitHubAPI = new GitHubAPI();
-    public static ConvertResponse convertResponse = new ConvertResponse();
-    public static FileWriterGson fileWriterGson = new FileWriterGson();
-    public static Scanner scanner = new Scanner(System.in);
-    public static String[] parts;
-    public static String githubUser = "";
-    public static String joinedInput;
 
     // TODO
     // ~~
 
-    static void main(String[] args) throws IOException, InterruptedException {
-        while (true) {
-            try {
-                System.out.print("> ");
-
-                githubUser = scanner.nextLine();
-                parts = githubUser.split("\\s+");
-
-                if (githubUser.equalsIgnoreCase("exit")) {
-                    break;
-                }
-
-                if (parts[0].equalsIgnoreCase("user-activity")) {
-                    joinedInput = String.join(" ", Arrays.copyOfRange(parts, 1, parts.length));
-                    joinedInput = joinedInput.replaceAll("^\"|\"$", "");
-                    githubUser = joinedInput;
-
-                    gitHubAPI.requestAPI(githubUser);
-                    convertResponse.formattedResponse();
-                    fileWriterGson.writeFile();
-
-                } else {
-                    System.out.println("Wrong command.");
-                }
-
-
-
-            } catch (IOException | InterruptedException | IllegalStateException | JsonSyntaxException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+    public static void main(String[] args) {
+        UserActivityLogic.fetchUser();
     }
 }
 
